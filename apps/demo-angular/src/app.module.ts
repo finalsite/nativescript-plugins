913365101479897cc5139cc549998f2c4b5cd7dc
Nick DeBaise <nick.debaise@finalsite.com>
2022-08-19 11:59:03 -0400

    Add webview import in demo instead of plugin

--- apps/demo-angular/src/app.module.ts
+++ apps/demo-angular/src/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from '@nativescript/angular';
+import {WebViewExtModule} from "@nota/nativescript-webview-ext/angular"
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,6 @@ import { HomeComponent } from './home.component';
 	schemas: [NO_ERRORS_SCHEMA],
 	declarations: [AppComponent, HomeComponent],
 	bootstrap: [AppComponent],
-	imports: [NativeScriptModule, AppRoutingModule],
+	imports: [NativeScriptModule, AppRoutingModule, WebViewExtModule],
 })
 export class AppModule {}
--- packages/rich-text-editor/angular/index.ts
+++ packages/rich-text-editor/angular/index.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { registerElement } from '@nativescript/angular';
 import { RichTextEditor } from '@finalsite/rich-text-editor';
-import {WebViewExtModule} from "@nota/nativescript-webview-ext/angular"
 
-@NgModule({
-    imports: [WebViewExtModule],
-})
+@NgModule()
 export class NativeScriptRichTextEditorModule {}
 
 registerElement('RichTextEditor', () => RichTextEditor);
