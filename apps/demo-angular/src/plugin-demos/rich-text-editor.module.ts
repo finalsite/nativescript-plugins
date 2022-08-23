41d304b9d6829149d60d0c64becbe03a7de5178b
Nick DeBaise <nick.debaise@finalsite.com>
2022-08-23 10:21:29 -0400

    AMP-42 Check HTML btn

--- apps/demo-angular/src/plugin-demos/rich-text-editor.module.ts
+++ apps/demo-angular/src/plugin-demos/rich-text-editor.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
 import { RichTextEditorComponent } from './rich-text-editor.component';
-import {NativeScriptRichTextEditorModule} from '@finalsite/rich-text-editor/angular'
+import { NativeScriptRichTextEditorModule } from '@finalsite/rich-text-editor/angular'
 
 @NgModule({
 	imports: [NativeScriptCommonModule, NativeScriptRichTextEditorModule, NativeScriptRouterModule.forChild([{ path: '', component: RichTextEditorComponent }])],
